ac3552321e704eae3eb4df5342e434e2d15c662d
Mandy Alber <mandy.alber@gmail.com>
2021-01-24 20:27:07 -0800

    update to multi page and add intro

---
+++ test.js
@@ -0,0 +1,26 @@
+const mandyAlber = {
+    title: 'Full Stack Software Developer',
+    front_end_skills: [
+        'JavaScript',
+        'React.js',
+        'jQuery',
+        'HTML5',
+        'CSS3'
+    ],
+    back_end_skills: [
+        'Node.js',
+        'Express.js',
+        'PostgreSQL'
+    ],
+    enjoys: [
+        'solving problems and puzzles',
+        'improving efficiency',
+        'automating repetitive tasks'
+    ]
+}
+
+console.log(
+    `Hi, I am a ${mandyAlber.title}. 
+    I have Front-End experience in ${mandyAlber.front_end_skills} and Back-End experience in ${mandyAlber.back_end_skills}. 
+    I enjoy ${mandyAlber.enjoys}. Welcome to my portfolio site!`
+)
